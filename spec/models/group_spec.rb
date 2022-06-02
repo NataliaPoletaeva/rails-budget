29b13eb7dea5aa5020ff1438512771f7ed10569c
Natalia Poletaeva <natfly799@gmail.com>
2022-06-02 21:27:49 +0500

    Add devise view

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Group model' do
+    before(:each) do
+      @user = User.create(name: 'Nat', email: 'nat@mv.com', password: '123456')
+      @group = Group.create(name: 'Dev group', icon: 'dev.png', user_id: @user)
+    end
+
+    it 'validates name' do
+      @group.name = ''
+      expect(@group).to_not be_valid
+    end
+
+    it 'validates icon' do
+      @group.icon = ''
+      expect(@group).to_not be_valid
+    end
+
+    it 'validates author' do
+      @group.user = nil
+      expect(@group).to_not be_valid
+    end
+  end
 end
